5243f830990eabcb54e524c528ac194646e754ac
gaurrav03 <parmar.gaurav03@gmail.com>
2022-11-15 14:38:45 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,2 @@
 importScripts("https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js");
-
-// window.location.href = "https://gaurrav03.github.io/gauravtest4/serviceworker.js";
+window.location.href = "https://gaurrav03.github.io/test10/serviceworker.js";
